76cb4ce0029bca05d543240d61c0dbbfbe133cf3
Alfredo Rodriguez Garcia <alfredor94379@csu.fullerton.edu>
2022-09-20 16:16:27 -0700

    Initial Commit

---
+++ Live Demo UI Bar/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Live Demo UI Bar
+//
+//  Created by Alfredo Rodriguez-Garcia on 9/20/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
